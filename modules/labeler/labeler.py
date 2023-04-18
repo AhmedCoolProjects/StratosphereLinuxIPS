1a9c9388ba00263207ee79eff29e6e67edd9accc
alya <alyaggomaa@gmail.com>
2023-04-18 03:35:45 +0200

    labeler: create labeled log file and dump the labeled flows there

--- modules/labeler/labeler.py
+++ modules/labeler/labeler.py
@@ -26,6 +26,19 @@ class Module(Module, multiprocessing.Process):
         self.c1 = __database__.subscribe('add_label')
         self.open_handles = {}
 
+
+    def open_labeled_logfile(self, logfile_name: str) -> TextIO:
+        """
+        opens the labeled log file placed inside the labeled_flows dir
+        and stores the handle in self.open_handles
+        creates the file if it isn't there
+        @param logfile_name: conn.log, dns.log, arp.log etc
+        """
+        labeled_logfile_path = os.path.join(self.labels_dir, f'{logfile_name}.labeled')
+        # create the file
+        open(labeled_logfile_path, 'w').close()
+        self.open_handles[logfile_name] = open(labeled_logfile_path, 'a')
+
     def init_labels_dir(self):
         """ creates the labeled_flows dir if not there """
         self.labels_dir = os.path.join(__database__.get_output_dir(), 'labeled_flows/')
@@ -48,7 +61,20 @@ class Module(Module, multiprocessing.Process):
         utils.drop_root_privs()
         while True:
             try:
-              pass
+                message = __database__.get_message(self.c1)
+                if message and message['data'] == 'stop_process':
+                    self.shutdown_gracefully()
+                    return True
+
+                if utils.is_msg_intended_for(message, 'add_label'):
+
+
+                    if file not in self.open_handles:
+                        self.open_labeled_logfile(file)
+                    labeled_log_file: TextIO = self.open_handles[file]
+
+                    json.dump(flow, labeled_log_file)
+                    labeled_log_file.write('\n')
 
 
             except KeyboardInterrupt:
